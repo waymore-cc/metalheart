34cd366b86c53ce3dd1bedd5c00773ef15eb307e
waymore-cc <pawel.kamyszek@waymore.cc>
2019-12-11 19:00:01 +0100

    Najlepiej jak dotad 2

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Wracamy do gry...`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Najlepsze charytatywne koncerty we Wrocławiu!`,
     author: `@gatsbyjs`,
   },
   plugins: [
