b3f21d556aa499fe58620b63ac92ed2c9b847cf9
waymore-cc <pawel.kamyszek@waymore.cc>
2019-12-11 14:46:03 +0100

    Z tabami

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Metalheart from "../images/metalheart.svg"
+import Tabs from "../components/Tabs"
 
 const IndexPage = () => (
   <Layout>
@@ -13,27 +14,39 @@ const IndexPage = () => (
     <div style={{ display: "flex", padding: "2rem 0 5rem 0" }}>
       <Metalheart style={{ margin: "auto", width: "40%" }} />
     </div>
-    <div
-      style={{
-        display: "grid",
-        gridTemplateColumns: "1fr 2fr",
-        gridGap: "2rem",
-        paddingBottom: "400px",
-      }}
-    >
-      <div>
-        <h2>Wracamy do gry...</h2>
+
+    <Tabs>
+      <div label="Metalheart">
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "1fr 2fr",
+            gridGap: "2rem",
+            padding: " ",
+          }}
+        >
+          <div>
+            <h2>O nas</h2>
+          </div>
+          <div>
+            <p>
+              Metalheart to projekt z kilkuletnią historią – przez 8 edycji wraz
+              z ekipą Ciemnej Strony Miasta łączyliśmy naszą miłość do muzyki z
+              chęcią pomagania podopiecznym fundacji Król i Smok. Po ponad
+              dwuletniej przerwie Metalheart powrócił, tym razem przy współpracy
+              z klubem D.K. Luksus.
+            </p>
+          </div>
+        </div>
       </div>
-      <div>
-        <p>
-          Metalheart to projekt z kilkuletnią historią – przez 8 edycji wraz z
-          ekipą Ciemnej Strony Miasta łączyliśmy naszą miłość do muzyki z chęcią
-          pomagania podopiecznym fundacji Król i Smok. Po ponad dwuletniej
-          przerwie Metalheart powrócił, tym razem przy współpracy z klubem D.K.
-          Luksus.
-        </p>
+      <div label="Wydarzenia">
+        After 'while, <em>Crocodile</em>!
       </div>
-    </div>
+      <div label="Kontakt">
+        Nothing to see here, this tab is <em>extinct</em>!
+      </div>
+    </Tabs>
+
     <div
       style={{
         display: "grid",
